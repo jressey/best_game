73fa70f7da0e4c5b91f2a3428040503309391972
Joe Essey <joe.essey@salesloft.com>
2018-09-30 15:12:27 -0400

    add passthrough to random github endpoint

--- lib/best_game_web/controllers/nfl_controller.ex
+++ lib/best_game_web/controllers/nfl_controller.ex
@@ -2,7 +2,13 @@ defmodule BestGameWeb.NflController do
   use BestGameWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", foo: get("https://api.github.com")
   end
 
+  def get(url, headers \\ []) do
+    case HTTPoison.get(url, headers) do
+      {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
 end
